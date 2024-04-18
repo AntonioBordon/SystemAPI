8a1e416146409c66f7deeb6bddfde0673ed70f6d
Antonio Bordon <Antonio Bordon@DESKTOP-B29LBUC>
2024-04-17 22:18:10 -0300

    Add project files.

---
+++ WindowsFormsApp1/FormBuscaCEP.Designer.cs
@@ -0,0 +1,436 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FormBuscaCEP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCNPJComprador = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCNPJFornecedor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumeroCotacao = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDataCotacao = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDataEntregaCotacao = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtLogradouro = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtObservacao = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtNumeroItem = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtDescricaoItem = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.txtUnidade = new System.Windows.Forms.TextBox();
+            this.bntEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCNPJComprador
+            // 
+            this.txtCNPJComprador.Location = new System.Drawing.Point(12, 42);
+            this.txtCNPJComprador.Name = "txtCNPJComprador";
+            this.txtCNPJComprador.Size = new System.Drawing.Size(145, 20);
+            this.txtCNPJComprador.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CNPJ Comprador";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CNPJ Fornecedor";
+            // 
+            // txtCNPJFornecedor
+            // 
+            this.txtCNPJFornecedor.Location = new System.Drawing.Point(163, 42);
+            this.txtCNPJFornecedor.Name = "txtCNPJFornecedor";
+            this.txtCNPJFornecedor.Size = new System.Drawing.Size(145, 20);
+            this.txtCNPJFornecedor.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(315, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Numero Cotacao";
+            // 
+            // txtNumeroCotacao
+            // 
+            this.txtNumeroCotacao.Location = new System.Drawing.Point(314, 42);
+            this.txtNumeroCotacao.Name = "txtNumeroCotacao";
+            this.txtNumeroCotacao.Size = new System.Drawing.Size(145, 20);
+            this.txtNumeroCotacao.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(466, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Data Cotacao";
+            // 
+            // txtDataCotacao
+            // 
+            this.txtDataCotacao.Location = new System.Drawing.Point(465, 42);
+            this.txtDataCotacao.Name = "txtDataCotacao";
+            this.txtDataCotacao.Size = new System.Drawing.Size(145, 20);
+            this.txtDataCotacao.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(617, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Data Entrega Cotacao";
+            // 
+            // txtDataEntregaCotacao
+            // 
+            this.txtDataEntregaCotacao.Location = new System.Drawing.Point(616, 42);
+            this.txtDataEntregaCotacao.Name = "txtDataEntregaCotacao";
+            this.txtDataEntregaCotacao.Size = new System.Drawing.Size(145, 20);
+            this.txtDataEntregaCotacao.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "CEP";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(12, 89);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(145, 20);
+            this.txtCep.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(164, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Logradouro";
+            // 
+            // txtLogradouro
+            // 
+            this.txtLogradouro.Location = new System.Drawing.Point(163, 89);
+            this.txtLogradouro.Name = "txtLogradouro";
+            this.txtLogradouro.Size = new System.Drawing.Size(145, 20);
+            this.txtLogradouro.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(315, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Complemento";
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(314, 89);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(145, 20);
+            this.txtComplemento.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(466, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Bairro";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(465, 89);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(145, 20);
+            this.txtBairro.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(617, 70);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "UF";
+            // 
+            // txtUF
+            // 
+            this.txtUF.Location = new System.Drawing.Point(616, 89);
+            this.txtUF.Name = "txtUF";
+            this.txtUF.Size = new System.Drawing.Size(145, 20);
+            this.txtUF.TabIndex = 18;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 122);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(65, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Observacao";
+            // 
+            // txtObservacao
+            // 
+            this.txtObservacao.Location = new System.Drawing.Point(12, 141);
+            this.txtObservacao.Name = "txtObservacao";
+            this.txtObservacao.Size = new System.Drawing.Size(145, 20);
+            this.txtObservacao.TabIndex = 20;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(13, 233);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(67, 13);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Numero Item";
+            // 
+            // txtNumeroItem
+            // 
+            this.txtNumeroItem.Location = new System.Drawing.Point(12, 252);
+            this.txtNumeroItem.Name = "txtNumeroItem";
+            this.txtNumeroItem.Size = new System.Drawing.Size(145, 20);
+            this.txtNumeroItem.TabIndex = 22;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(168, 233);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(78, 13);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Descrição Item";
+            // 
+            // txtDescricaoItem
+            // 
+            this.txtDescricaoItem.Location = new System.Drawing.Point(167, 252);
+            this.txtDescricaoItem.Name = "txtDescricaoItem";
+            this.txtDescricaoItem.Size = new System.Drawing.Size(145, 20);
+            this.txtDescricaoItem.TabIndex = 24;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(319, 233);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(35, 13);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "Preço";
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(318, 252);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(145, 20);
+            this.txtPreco.TabIndex = 26;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(470, 233);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(62, 13);
+            this.label15.TabIndex = 29;
+            this.label15.Text = "Quantidade";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(469, 252);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(145, 20);
+            this.txtQuantidade.TabIndex = 28;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(621, 233);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(37, 13);
+            this.label16.TabIndex = 31;
+            this.label16.Text = "Marca";
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(620, 252);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(145, 20);
+            this.txtMarca.TabIndex = 30;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(13, 289);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(47, 13);
+            this.label17.TabIndex = 33;
+            this.label17.Text = "Unidade";
+            // 
+            // txtUnidade
+            // 
+            this.txtUnidade.Location = new System.Drawing.Point(12, 308);
+            this.txtUnidade.Name = "txtUnidade";
+            this.txtUnidade.Size = new System.Drawing.Size(145, 20);
+            this.txtUnidade.TabIndex = 32;
+            // 
+            // bntEnviar
+            // 
+            this.bntEnviar.Location = new System.Drawing.Point(636, 308);
+            this.bntEnviar.Name = "bntEnviar";
+            this.bntEnviar.Size = new System.Drawing.Size(94, 33);
+            this.bntEnviar.TabIndex = 34;
+            this.bntEnviar.Text = "Enviar";
+            this.bntEnviar.UseVisualStyleBackColor = true;
+            this.bntEnviar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormBuscaCEP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 353);
+            this.Controls.Add(this.bntEnviar);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.txtUnidade);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtDescricaoItem);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtNumeroItem);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtObservacao);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtLogradouro);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDataEntregaCotacao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDataCotacao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumeroCotacao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCNPJFornecedor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCNPJComprador);
+            this.Name = "FormBuscaCEP";
+            this.Text = "Busca CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCNPJComprador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCNPJFornecedor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumeroCotacao;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDataCotacao;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDataEntregaCotacao;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtLogradouro;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtUF;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtObservacao;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtNumeroItem;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtDescricaoItem;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox txtUnidade;
+        private System.Windows.Forms.Button bntEnviar;
+    }
+}
+
---
+++ WindowsFormsApp1/FormBuscaCEP.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormBuscaCEP : Form
+    {
+        public FormBuscaCEP()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (txtCNPJComprador.Text == "")
+            {
+                MessageBox.Show("Campo CNPJ Comprador é obrigatorio");
+            }
+            if (txtCNPJFornecedor.Text == "")
+            {
+                MessageBox.Show("Campo CNPJ Fornecedor é obrigatorio");
+            }
+            if (txtNumeroCotacao.Text == "")
+            {
+                MessageBox.Show("Campo Numero Cotacao é obrigatorio");
+            }
+            if (txtDataCotacao.Text == "")
+            {
+                MessageBox.Show("Campo Data Cotacao é obrigatorio");
+            }
+            if (txtDataEntregaCotacao.Text == "")
+            {
+                MessageBox.Show("Campo Data Entrega Cotacao é obrigatorio");
+            }
+            if (txtCep.Text == "")
+            {
+                MessageBox.Show("Campo CEP é obrigatorio");
+            }
+            if (txtQuantidade.Text == "")
+            {
+                MessageBox.Show("Campo Quantidade é obrigatorio");
+            }
+            if (txtNumeroItem.Text == "")
+            {
+                MessageBox.Show("Campo Numero Item é obrigatorio");
+            }
+            if (txtDescricaoItem.Text == "")
+            {
+                MessageBox.Show("Campo Descrição Item é obrigatorio");
+            }
+            else
+            {
+                var cep = txtCep.Text;
+                var endereco = await BuscarCEPAsync(cep);
+                if (txtLogradouro.Text != "")
+                {
+                    if(txtUF.Text != "")
+                    {
+                        if (txtBairro.Text != "")
+                        {
+                            if(txtComplemento.Text != "")
+                            {
+                                return;
+                            }
+                            else
+                            {
+                                txtComplemento.Text = endereco.complemento;
+                            }
+                        }
+                        else
+                        {
+                            txtBairro.Text = endereco.bairro;
+                        }
+
+                    }
+                    else
+                    {
+                        txtUF.Text = endereco.uf;
+                    }
+                }
+                else
+                {
+                    txtLogradouro.Text = endereco.logradouro;
+                }
+            }
+        }
+        private async Task<Endereco> BuscarCEPAsync(string cep)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var response = await httpClient.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+
+                        var content = await response.Content.ReadAsStringAsync();
+                        var endereco = JsonSerializer.Deserialize<Endereco>(content);
+
+                        return endereco;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao buscar CEP: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+        }
+    }
+
